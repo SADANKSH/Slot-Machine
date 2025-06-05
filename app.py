5cd0dd0a183fbfe768d345d52eea76b73e2906a7
Sadanksh Gangrade <sadankshzz@hotmail.com>
2025-06-05 18:27:35 +0530

    Added Lines Feature

--- app.py
+++ app.py
@@ -12,6 +12,20 @@ def deposit():
 
     return amount
 
+def get_lines():
+    while True:
+        lines = input("Please enter the number of lines you want to  bet on 1-3 : ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 0<lines<=3:
+                break
+            else:
+                print("Lines must be greater than 0.")
+        else:
+            print("Please enter a valid number for valid number of lines")
+
+    return lines
+
 def main():
     balance=deposit()
     print(f"You have ${balance} in your wallet.")
